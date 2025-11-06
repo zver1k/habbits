20c224e4720872d0ac088dad4a57fce9467f9b96
Damir <mail@damirmedia.ru>
2025-11-06 17:24:45 +0500

    Верстка макета 2 этап

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY'
+
+// utils
+
+function loadData() {
+  const habbitsString = localStorage.getItem('HABIT_KEY');
+  const habbitArray = JSON.parse(habbitsString);
+  if (Array.isArray(habbitArray)) {
+    habbits = habbitArray;
+  }
+}
+
+function saveData() {
+  localStorage.setItem('HABBIT_KEY', JSON.stringify(habbits));
+}
+
+(() => {
+  loadData();
+})();
